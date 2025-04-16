7be66b94e2ef411cae8a9703c88ccbb95745413a
Aqib <105609780+AqibMiah@users.noreply.github.com>
2025-04-16 13:03:09 +0100

    Base Swift files

---
+++ MutqinHifdhApp/MutqinHifdhApp/MutqinHifdhAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutqinHifdhAppApp.swift
+//  MutqinHifdhApp
+//
+//  Created by Aqib Miah on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MutqinHifdhAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MutqinHifdhApp/MutqinHifdhAppTests/MutqinHifdhAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MutqinHifdhAppTests.swift
+//  MutqinHifdhAppTests
+//
+//  Created by Aqib Miah on 16/04/2025.
+//
+
+import Testing
+@testable import MutqinHifdhApp
+
+struct MutqinHifdhAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
